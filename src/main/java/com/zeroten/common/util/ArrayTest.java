a4247ca8802b2cb3fa09232fc7bb087eb534b162
何文东 <1368814309@qq.com>
2019-12-17 10:54:02 +0800

    课堂案例——数组拷贝、fill

--- src/main/java/com/zeroten/common/util/ArrayTest.java
+++ src/main/java/com/zeroten/common/util/ArrayTest.java
@@ -69,5 +69,47 @@ public class ArrayTest {
         });
     }
 
+    @Test
+    public void testCopy(){
+        //数组的复制
+        Integer[] arr1={1,2,3,4,5};
+        Integer[] arr2={6,7,8,9,10};
+        //拷贝数组arr1的前三个元素生成一个新数组
+        Integer[] arr11=new Integer[3];
+        arr11[0]=arr1[0];
+        arr11[1]=arr1[1];
+        arr11[2]=arr1[2];
+        System.out.println("arr11="+Arrays.toString(arr11));
+        Integer[] arr12=Arrays.copyOf(arr1,3);
+        System.out.println("arr12="+Arrays.toString(arr12));
 
+          //拷贝数组arr1的后三位元素到一个新数组
+        Integer[] arr13=Arrays.copyOfRange(arr1,arr1.length-3,arr1.length);
+        System.out.println("arr13="+Arrays.toString(arr13));
+
+        //拷贝数组arr1的第一到第三元素（不包括第三）
+        Integer[] arr14=Arrays.copyOfRange(arr1,0,2);
+        System.out.println("arr14="+Arrays.toString(arr14));
+
+        //拷贝数组arr2的后三位到arr1的后三位
+        System.arraycopy(arr2,arr2.length-3,arr1,arr1.length-3,3);
+        System.out.println("arr1="+Arrays.toString(arr1));
+
+    }
+
+    @Test
+    public void testFill(){
+        //实例：定义一个大小为10的int数组，并将数组初步初始化为指定值5，打印数组
+        //然后将数组后三赋值为3，再次打印
+        int[] arr=new int[10];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=5;
+        }
+        System.out.println(Arrays.toString(arr));
+
+        for(int i=arr.length-3;i<arr.length;i++){
+            arr[i]=3;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
