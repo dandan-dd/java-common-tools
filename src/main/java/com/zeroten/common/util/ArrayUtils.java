c0819c8c2b1e1b0df3e0da21710a63dde3de9b2e
何文东 <1368814309@qq.com>
2019-12-17 16:26:01 +0800

    数组——课后练习

--- src/main/java/com/zeroten/common/util/ArrayUtils.java
+++ src/main/java/com/zeroten/common/util/ArrayUtils.java
@@ -1,4 +1,24 @@
 package com.zeroten.common.util;
 
+import java.util.Arrays;
+
 public class ArrayUtils {
+    public static int[] sort(int[] arr){
+        System.out.println(Arrays.toString(arr));
+        int mix;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    mix=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=mix;
+                }
+            }
+        }
+        return arr;
+    }
+        public static void main (String[] args){
+        int[] arr={9,8,7,6,5,4,3,2,1};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
 }
